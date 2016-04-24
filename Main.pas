b4e5a35ccf79ab663366babc1bc79926f9e4867a
SwSl <anto14juillet@hotmail.fr>
2016-04-25 01:30:12 +0200

    Here Comes The Main Menu!
    First Stable Release

---
+++ Main.pas
@@ -0,0 +1,57 @@
+unit Main;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, jpeg, ExtCtrls, Menus, Unit1;
+
+type
+  TForm2 = class(TForm)
+    MainMenu1: TMainMenu;
+    Fichier1: TMenuItem;
+    Aide1: TMenuItem;
+    VersionWeb1: TMenuItem;
+    VersionWindows1: TMenuItem;
+    NouvellePartie1: TMenuItem;
+    N1: TMenuItem;
+    checs1: TMenuItem;
+    Shogi1: TMenuItem;
+    CommingSoon1: TMenuItem;
+    N2: TMenuItem;
+    Aide2: TMenuItem;
+    Quitter1: TMenuItem;
+    Paramtres1: TMenuItem;
+    Langue1: TMenuItem;
+    Image1: TImage;
+    Image2: TImage;
+    procedure LaunchChess(Sender: TObject);
+    procedure LaunchShogi(Sender: TObject);
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.LaunchChess(Sender: TObject);
+begin
+	Form2.Hide;
+	Application.CreateForm(TForm1, Form1);
+	Form1.ShowModal;
+	Form1.Free;
+	Form2.Show;
+end;
+
+procedure TForm2.LaunchShogi(Sender: TObject);
+begin
+	{comment to avoid deletion}
+end;
+
+end.
--- Project2.dpr
+++ Project2.dpr
@@ -2,13 +2,15 @@ program Project2;
 
 uses
   Forms,
-  Unit1 in 'Unit1.pas' {Form1};
+  Unit1 in 'Unit1.pas' {Form1},
+  Main in 'Main.pas' {Form2};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.Title := 'Chess Engine';
+  Application.Title := 'Chess Engine';  
+  Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
